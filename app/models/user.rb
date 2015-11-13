e7a0b67c185bf0476222edfdb8061301e1b6501c
TagirovAlbert <bert_tagirov@mail.ru>
2015-11-13 21:45:45 +0300

    Update correct sendig mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,:omniauthable, :confirmable,
+  devise :database_authenticatable, :registerable,:omniauthable, #:confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :events
 
